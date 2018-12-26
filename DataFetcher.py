d05b0624ff740afc5e95474195f25abab688a059
hallblazzar <hallblazzar@gmail.com>
2018-12-26 17:18:17 +0800

    Project initialization.

---
+++ DataFetcher.py
@@ -0,0 +1,91 @@
+import os
+import requests
+from datetime import datetime
+from dateutil.rrule import rrule, DAILY
+import re
+import gzip
+import shutil
+import time
+
+
+class DataFetcher:
+    def __init__(self, base_url: str, start_date: datetime, end_date: datetime, target_dir: str):
+        self.__base_url = base_url
+        self.__start_date = start_date
+        self.__end_date = end_date
+        self.__target_dir = target_dir
+
+    def execute(self, post_action=None):
+        for date in list(rrule(freq=DAILY, dtstart=self.__start_date, until=self.__end_date, interval=1)):
+            target_url = self.__get_url(date)
+            http_response = self.__fetch_data_from_target_url_through_date(target_url)
+            file_path = self.__write_http_content_to_file(http_response=http_response, target_url=target_url)
+            print("{} saved.".format(file_path))
+            if post_action is not None:
+                post_action(file_path)
+
+            time.sleep(2)
+
+    def __get_url(self, date):
+        date_in_string = date.strftime("%Y%m%d")
+        target_url = self.__base_url.format(date_in_string)
+
+        return target_url
+
+    def __fetch_data_from_target_url_through_date(self, target_url: str):
+        print("download: {}".format(target_url))
+        response = requests.get(target_url)
+        print("done")
+
+        return response
+
+    def __write_http_content_to_file(self, http_response, target_url: str):
+        file_name = target_url.rsplit("/", 1)[1]
+        file_path = os.path.join(self.__target_dir, file_name)
+
+        with open(file_path, 'wb') as file:
+            file.write(http_response.content)
+
+        return file_path
+
+
+class DataExtractor:
+    def execute(self, source_file_path):
+        target_file_path = self.__get_target_file_path(source_file_path)
+
+        with gzip.open(source_file_path, 'rb') as gzip_file:
+            with open(target_file_path, 'wb') as unzipped_file:
+                shutil.copyfileobj(gzip_file, unzipped_file)
+
+        print("extract {} to {}.".format(source_file_path, target_file_path))
+
+    def __get_target_file_path(self, source_file_path):
+        source_file_name = os.path.basename(source_file_path)
+        split_source_file_name = os.path.splitext(source_file_name)
+
+        filename_without_extension = split_source_file_name[0]
+        extracted_extension = split_source_file_name[1]
+        target_file_name = "{}{}".format(filename_without_extension, extracted_extension)
+
+        target_file_path = os.path.join(os.path.dirname(source_file_path), target_file_name)
+
+        return target_file_path
+
+
+if __name__ == '__main__':
+    target_dir = os.path.join("news")
+    os.makedirs(target_dir, exist_ok=True)
+
+    zipped_base_url = "http://g0v-data.gugod.org/people-in-news/db/articles-{}.jsonl.gz"
+    zipped_start_date = datetime(2018, 10, 19)
+    zipped_end_date = datetime(2018, 12, 20)
+    DataFetcher(
+        base_url=zipped_base_url, start_date=zipped_start_date, end_date=zipped_end_date, target_dir=target_dir
+    ).execute()
+
+    # unzipped_base_url = "http://g0v-data.gugod.org/people-in-news/db/articles-{}.jsonl"
+    # unzipped_start_date = datetime(2018, 12, 18)
+    # unzipped_end_date = datetime(2018, 12, 20)
+    # DataFetcher(
+    #     base_url=unzipped_base_url, start_date=unzipped_start_date, end_date=unzipped_end_date, target_dir=target_dir
+    # ).execute()
---
+++ JsonAnalyzer.py
@@ -0,0 +1,109 @@
+import json
+import jieba
+import jieba.analyse
+import uuid
+import os
+import re
+from tqdm import tqdm
+import traceback
+
+
+def fetch_news_file(news_dir: str):
+    news_file_list = [
+        os.path.join(news_dir, file) for file in os.listdir(news_dir) if os.path.isfile(os.path.join(news_dir, file))
+    ]
+
+    news_file_list = [
+        file for file in news_file_list if os.path.splitext(os.path.basename(file))[-1] != ".gz"
+    ]
+
+    return news_file_list
+
+
+class NewsFileConverter:
+    def __init__(self, news_file_path: str, converted_news_dir_path: str):
+        self.__news_file_path = news_file_path
+        self.__converted_news_dir_path = converted_news_dir_path
+
+        self.__news_json_list = list()
+        self.__keyword_json_list = list()
+
+    def execute(self):
+        news_json_string_list = self.__fetch_news_json_string()
+        date_string = re.findall(r"[0-9]{8}", os.path.basename(news_file))[0]
+        self.__convert_all_news_json_string(news_json_string_list=news_json_string_list, date_string=date_string)
+        self.__save_json_to_file(date_string)
+
+    def __fetch_news_json_string(self):
+        with open(self.__news_file_path, "r", encoding='utf-8') as file:
+            news_json_string_list = file.readlines()
+
+        return news_json_string_list
+
+    def __convert_all_news_json_string(self, news_json_string_list: list, date_string: str):
+        for news_json_string in tqdm(news_json_string_list):
+            news_json, keyword_json = SingleNewsContentConverter().execute(
+                news_json_string=news_json_string,
+                date_string=date_string
+            )
+
+            self.__news_json_list.append(news_json)
+            self.__keyword_json_list.append(keyword_json)
+
+    def __save_json_to_file(self, date_string: str):
+        converted_news_file_path = os.path.join(self.__converted_news_dir_path, "{}-article.json".format(date_string))
+        keyword_path = os.path.join(self.__converted_news_dir_path, "{}-keyword.json".format(date_string))
+
+        self.__write_json_to_file(json_object=self.__news_json_list, file_path=converted_news_file_path)
+        self.__write_json_to_file(json_object=self.__keyword_json_list, file_path=keyword_path)
+
+    @staticmethod
+    def __write_json_to_file(json_object: list, file_path: str):
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(json_object, file, ensure_ascii=False)
+
+        print("{} saved.".format(file_path))
+
+
+class SingleNewsContentConverter:
+    def execute(self, news_json_string: str, date_string: str):
+        article_id = "{}-{}".format(date_string, uuid.uuid4())
+
+        news_json = json.loads(news_json_string)
+        news_json['article_id'] = article_id
+
+        keyword_json = self.__fetch_keyword_json(content_text=news_json['content_text'], article_id=article_id)
+
+        return news_json, keyword_json
+
+    @staticmethod
+    def __fetch_keyword_json(content_text: str, article_id: str) -> dict:
+        keyword_list = jieba.analyse.extract_tags(content_text, topK=10, withWeight=True)
+        keyword_json = {"article_id": article_id, "keyword_list": list()}
+
+        for keyword, weight in keyword_list:
+            keyword_json["keyword_list"].append({"keyword": keyword, "weight": weight})
+
+        return keyword_json
+
+
+if __name__ == '__main__':
+    moe_dict_path = os.path.join("dict", "moe_expand.dict")
+    stop_word_dict_path = os.path.join("dict", "stopping_words.dict")
+    news_dir = os.path.join("news")
+    converted_news_dir = os.path.join("converted_news")
+
+    jieba.load_userdict(moe_dict_path)
+    jieba.analyse.set_stop_words(stop_word_dict_path)
+
+    news_file_list = fetch_news_file(news_dir)
+    os.makedirs(converted_news_dir, exist_ok=True)
+
+    for news_file in news_file_list:
+        try:
+            print("converting: {}".format(news_file))
+            NewsFileConverter(news_file_path=news_file, converted_news_dir_path=converted_news_dir).execute()
+        except:
+            print("convert: {} failed.")
+            print(traceback.format_exc())
+
